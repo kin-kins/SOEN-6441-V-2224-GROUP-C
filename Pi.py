6b3742f81aa7f955e59f6dc3e4e9dffcf567bcff
ashu <k.ashu403@gmail.com>
2023-03-06 10:55:49 -0500

    Added classes

--- Pi.py
+++ Pi.py
@@ -1,25 +1,23 @@
 import random
 
-def estimate_pi(n):
-    # Initialize a counter to keep track of the number of points that fall inside the unit circle
-    num_points_inside_circle = 0
-    # Total number of points generated
-    num_points_total = n
-    # Loop through n times to generate n random points and check if each point falls inside the unit circle
-    for i in range(n):
-        # Generate a random x-coordinate and y-coordinate between 0 and 1
-        x = random.uniform(0, 1)
-        y = random.uniform(0, 1)
-        # Calculate the squared distance from the origin to the point
-        distance_squared = x**2 + y**2
-        # Check if the point falls inside the unit circle (distance from origin <= 1)
-        if distance_squared <= 1:
-            # If the point is inside the unit circle, increment the counter
-            num_points_inside_circle += 1
-    # Calculate the estimated value of pi using the ratio of the number of points inside the circle to the total number of points
-    pi_estimate = 4 * num_points_inside_circle / num_points_total
-    # Return the estimated value of pi
-    return pi_estimate
+class PiEstimator:
+    def __init__(self, n):
+        self.n = n
+        self.num_points_inside_circle = 0
+        self.num_points_total = n
 
-# Call the function to estimate pi using 1,000,000 random points and print the result
-print(estimate_pi(1000000))
+    def estimate_pi(self):
+        for i in range(self.n):
+            x = random.uniform(0, 1)
+            y = random.uniform(0, 1)
+            distance_squared = x**2 + y**2
+            if distance_squared <= 1:
+                self.num_points_inside_circle += 1
+        pi_estimate = 4 * self.num_points_inside_circle / self.num_points_total
+        return pi_estimate
+
+# Create a PiEstimator object with n = 1000000
+estimator = PiEstimator(1000000)
+
+# Call the estimate_pi method and print the result
+print(estimator.estimate_pi())
