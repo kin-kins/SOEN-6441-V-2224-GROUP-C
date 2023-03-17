ae7c55630d6fe5398191e61792d449d0e5ab310d
Stan <stanislas.kiesgen.de.richter@efrei.net>
2023-03-17 16:10:13 -0400

    docstring comments and exception handling

--- Coasters_overlap.py
+++ Coasters_overlap.py
@@ -2,20 +2,62 @@ from Handmade_math import HandmadeMath
 
 
 class Coasters_overlap:
+    """
+    A class that represents two overlapping coasters on a table.
+
+    Attributes:
+        pi (float): the value of pi generated using Monte Carlo simulation.
+        alpha (float): the angle between the two coasters, in radians, calculated
+            using the Newton-Raphson method for alpha - sin(alpha) = pi/2.
+        length (float): the length of the segment that connects the centers of the
+            two circles, calculated as 2r(1 - cos(alpha/2)), where r is the radius
+            of the coasters.
+        r (float): the radius of the coasters, as entered by the user.
+
+    Methods:
+        __init__(self): initializes an instance of the Coasters_overlap class.
+        raphson_method(self): calculates the value of alpha using the Newton-Raphson
+            method with 1000 iterations or until the error tolerance is reached.
+        to_string(self): returns a string representation of the Coasters_overlap object.
+    """
     pi = 0.0
     alpha = 0.0
-    l = 0.0
+    length = 0.0
     r = 0.0
 
     def __init__(self):
+        """
+        Initializes an instance of the HandmadeMath class.
+
+        The value of pi is generated using Monte Carlo simulation.
+        Alpha is the result of the Newton-Raphson method for alpha - sin(alpha) = pi/2.
+        radius r is asked to the user
+        and the length is calculated from the other elements using the equation l= 2r(1-cos(alpha/2))
+        """
         self.pi = HandmadeMath.estimate_pi(1000000)
         self.raphson_method()
+        self.r = float(input("Please enter the radius of the circles: "))
+        if self.r < 0:
+            raise Exception("Sorry, no numbers below zero for radius")
+        self.length = 2 * self.r * (1 - HandmadeMath.cos(self.alpha / 2))
 
     def raphson_method(self):
+        """
+        Calculates the value of alpha using the Newton-Raphson method with 1000 iterations or if the error tolerance is
+        reached
+
+        Local variables:
+            alpha (float): value of alpha initialized at pi/2.
+            tolerance(float) : set at 1e-6
+
+        Returns:
+            None.
+
+        Set Object alpha value to the estimation of the root of -> alpha - sin(alpha) = pi/2.
+        """
         alpha = self.pi / 2  # initial guess
         self.alpha = alpha
         tolerance = 1e-6
-        self.r = float(input("Enter the radius of the circles: "))
 
         def f(a):
             return a - HandmadeMath.sin(a) - self.pi / 2
@@ -24,15 +66,23 @@ class Coasters_overlap:
             return 1 - HandmadeMath.cos(a)
 
         # Calculating alpha using newton Raphson method
-
         for i in range(1000):
             alpha_new = self.alpha - f(self.alpha) / f_derivative(self.alpha)
             if abs(alpha_new - self.alpha) < tolerance:
                 break
             self.alpha = alpha_new
-        self.l = 2 * self.r * (1 - HandmadeMath.cos(self.alpha / 2))  # length of segment X1X2
 
     def to_string(self):
-        s = f"The length of segment X1X2 is: {self.l}\n with alpha =  {self.alpha}\n r =  {self.r}\n pi =  {self.pi} \n"
+        """
+            Returns a string representation of the HandmadeMath object.
+
+            Parameters:
+                    Coasters_overlap object (self)
+
+            Returns:
+                    s (str): A string representation of the HandmadeMath object,
+                            including the length of segment X1X2, alpha, r, and pi.
+        """
+        s = f"The length of segment X1X2 is: {self.length}\n with alpha =  {self.alpha}\n r =  {self.r}\n pi =  {self.pi} \n"
         return s
 
--- Handmade_math.py
+++ Handmade_math.py
@@ -4,10 +4,22 @@ import random
 class HandmadeMath:
     @staticmethod
     def estimate_pi(n):
+        """
+        Calculates an estimate of the value of pi using Monte Carlo simulation.
+
+        Parameters:
+            n (int): The number of points to be generated for the simulation.
+
+        Raises:
+            Exception: if n is not a positive integer
+
+        Returns:
+            pi_estimate (float): The estimated value of pi.
+        """
+        if not (type(n) == int):
+            raise Exception("input of estimate_pi needs to be a positive integer")
         # Initialize a counter to keep track of the number of points that fall inside the unit circle
         num_points_inside_circle = 0
-        # Total number of points generated
-        num_points_total = n
         # Loop through n times to generate n random points and check if each point falls inside the unit circle
         for estimate in range(n):
             # Generate a random x-coordinate and y-coordinate between 0 and 1
@@ -21,12 +33,25 @@ class HandmadeMath:
                 num_points_inside_circle += 1
         # Calculate the estimated value of pi using the ratio of the number
         # of points inside the circle to the total number of points
-        pi_estimate = 4 * num_points_inside_circle / num_points_total
-        # Return the estimated value of pi
+        pi_estimate = 4 * num_points_inside_circle / n
         return pi_estimate
 
     @staticmethod
-    def factorial(x):  # recursive calculation of factorial
+    def factorial(x):
+        """
+        Calculates the factorial of x recursively.
+
+        Parameters:
+            x (int): The number whose factorial needs to be calculated.
+
+        Raises:
+            Exception: if x is not a positive integer or x is less than zero.
+
+        Returns:
+            factorial (int): The factorial of x calculated recursively.
+        """
+        if not (type(x) == int):
+            raise Exception("input of factorial needs to be a positive integer")
         if x < 0:
             raise Exception("Sorry, no numbers below zero for factorials")
         if x == 0:
@@ -37,14 +62,32 @@ class HandmadeMath:
             return HandmadeMath.factorial(x - 1) * x
 
     @staticmethod
-    def cos(x, n=10):  # Calculation of cos(x) using taylor series with n(default = 10) elements
+    def cos(x, n=10):
+        """
+        Calculates the cosine of x using Taylor series with n (default = 10) elements.
+        Parameters:
+            x (float): The value in radians for which cosine needs to be calculated
+            n (int): The number of terms to be used in Taylor series. Default is 10.
+        Returns:
+            result (float): The value of cosine of x calculated using Taylor series
+        """
+        if (not (type(n) == int)) or n <= 0:
+            raise Exception("input n of cos needs to be a positive integer")
         result = 0
         for i in range(0, n + 1):
             result += ((-1) ** i) * ((x ** (2 * i)) / (HandmadeMath.factorial(2 * i)))
         return result
 
     @staticmethod
-    def sin(x, n=10):  # sin(x) = x - x^3/3! + x^5/5! - x^7/7! + ...   taylor series
+    def sin(x, n=10):
+        """
+        Calculates the sine of x using Taylor series with n (default = 10) elements.
+        Parameters:
+            x (float): The value in radians for which sine needs to be calculated
+            n (int): The number of terms to be used in Taylor series. Default is 10.
+        Returns:
+            result (float): The value of sine of x calculated using Taylor series
+        """
         result = 0
         sign = 1
         for i in range(1, 2 * n, 2):
@@ -52,25 +95,3 @@ class HandmadeMath:
             result += term
             sign *= -1
         return result
-
-
-'''
-    # Call the function to estimate pi using 1,000,000 random points and print the result
-    pi = estimate_pi(1000000)
-
-    alpha = pi / 2  # intial guess
-    tolerance = 1e-6
-    R = float(input("Enter the radius of the circles: "))
-
-
-# Calculating alpha using newton raphson method
-
-for i in range(1000):
-    alpha_new = alpha - f(alpha) / f_derivative(alpha)
-    if abs(alpha_new - alpha) < tolerance:
-        break
-    alpha = alpha_new
-
-l = 2*R*(1 - cos(alpha/2))  # length of segment X1X2
-print(f"The length of segment X1X2 is: {l}")
-'''
