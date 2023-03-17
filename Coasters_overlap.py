67b57372df22c218d3976b17479863b4586e5866
mehtavandit <65803900+mehtavandit@users.noreply.github.com>
2023-03-17 17:05:33 -0400

    Add files via upload

--- Coasters_overlap.py
+++ Coasters_overlap.py
@@ -4,7 +4,6 @@ from Handmade_math import HandmadeMath
 class Coasters_overlap:
     """
     A class that represents two overlapping coasters on a table.
-
     Attributes:
         pi (float): the value of pi generated using Monte Carlo simulation.
         alpha (float): the angle between the two coasters, in radians, calculated
@@ -13,7 +12,6 @@ class Coasters_overlap:
             two circles, calculated as 2r(1 - cos(alpha/2)), where r is the radius
             of the coasters.
         r (float): the radius of the coasters, as entered by the user.
-
     Methods:
         __init__(self): initializes an instance of the Coasters_overlap class.
         raphson_method(self): calculates the value of alpha using the Newton-Raphson
@@ -28,7 +26,6 @@ class Coasters_overlap:
     def __init__(self):
         """
         Initializes an instance of the HandmadeMath class.
-
         The value of pi is generated using Monte Carlo simulation.
         Alpha is the result of the Newton-Raphson method for alpha - sin(alpha) = pi/2.
         radius r is asked to the user
@@ -45,14 +42,11 @@ class Coasters_overlap:
         """
         Calculates the value of alpha using the Newton-Raphson method with 1000 iterations or if the error tolerance is
         reached
-
         Local variables:
             alpha (float): value of alpha initialized at pi/2.
             tolerance(float) : set at 1e-6
-
         Returns:
             None.
-
         Set Object alpha value to the estimation of the root of -> alpha - sin(alpha) = pi/2.
         """
         alpha = self.pi / 2  # initial guess
@@ -75,14 +69,17 @@ class Coasters_overlap:
     def to_string(self):
         """
             Returns a string representation of the HandmadeMath object.
-
             Parameters:
                     Coasters_overlap object (self)
-
             Returns:
                     s (str): A string representation of the HandmadeMath object,
                             including the length of segment X1X2, alpha, r, and pi.
         """
-        s = f"The length of segment X1X2 is: {self.length}\n" #with alpha =  {self.alpha}\n r =  {self.r}\n pi =  {self.pi} \n"
+        s = f"The radius of the circle is {self.r},and  length of segment X1X2 is: {self.length}\n" #with alpha =  {self.alpha}\n r =  {self.r}\n pi =  {self.pi} \n"
         return s
 
+    def get_radius(self):
+        return self.r
+
+    def get_length(self):
+        return self.length
