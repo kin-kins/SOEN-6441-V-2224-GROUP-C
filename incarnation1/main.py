5c88b1e371b54d8d46dcb0b705e939200baff254
ashu <k.ashu403@gmail.com>
2023-03-30 22:28:59 -0400

    Added main function to all the main.py files

--- incarnation1/main.py
+++ incarnation1/main.py
@@ -1,36 +1,37 @@
 import xml.etree.ElementTree as ET
 import Coasters_overlap
 
-# text file
-try:
-    file1 = open("../incarnation1/output.txt", 'w') #
-except (Exception,):
-    raise Exception("Cannot open a txt file")
+def main():
+    # text file
+    try:
+        file1 = open("../incarnation1/output.txt", 'w') #
+    except (Exception,):
+        raise Exception("Cannot open a txt file")
 
 
 
 
-while True:
     while True:
-        try:
-            coasters_pair_amount = int(input("Please enter the number of coaster pairs you want :"))
+        while True:
+            try:
+                coasters_pair_amount = int(input("Please enter the number of coaster pairs you want :"))
+                break
+            except ValueError:
+                print("this is not a valid number, try again")
+        if coasters_pair_amount < 1:
+            print("please input a positive amount of pairs")
+        else:
             break
-        except ValueError:
-            print("this is not a valid number, try again")
-    if coasters_pair_amount < 1:
-        print("please input a positive amount of pairs")
-    else:
-        break
 
-for i in range(coasters_pair_amount):
-    Coaster = Coasters_overlap.Coasters_overlap()
-    s = Coaster.to_string()
-    r = str(Coaster.get_radius())
-    L = str(Coaster.get_length())
-    file1.write(f"r={r}, L={L}\n")
+    for i in range(coasters_pair_amount):
+        Coaster = Coasters_overlap.Coasters_overlap()
+        s = Coaster.to_string()
+        r = str(Coaster.get_radius())
+        L = str(Coaster.get_length())
+        file1.write(f"r={r}, L={L}\n")
+
+    print("Done! Check your output.txt file for the results")
 
 
-
-
-
-print("Done! Check your output.txt file for the results")
+if __name__=="__main__":
+    main()
--- incarnation2/main.py
+++ incarnation2/main.py
@@ -1,45 +1,46 @@
 import xml.etree.ElementTree as ET
 import Coasters_overlap
 
+def main():
+    try:
+        file2 = open("Output.xml", 'wb')  # Opening a file, with operation mode `wb` (write + binary) #../incarnation2/
+    except (Exception,):
+        raise Exception("Cannot open an xml file")
+    # xml file
+    data = ET.Element('data')  # parent root element onto which other tags would be created
+    element1 = ET.SubElement(data, "list")  # adding a subtag name items inside root tag
 
-try:
-    file2 = open("Output.xml", 'wb')  # Opening a file, with operation mode `wb` (write + binary) #../incarnation2/
-except (Exception,):
-    raise Exception("Cannot open an xml file")
-# xml file
-data = ET.Element('data')  # parent root element onto which other tags would be created
-element1 = ET.SubElement(data, "list")  # adding a subtag name items inside root tag
-
-while True:
     while True:
-        try:
-            coasters_pair_amount = int(input("Please enter the number of coaster pairs you want :"))
+        while True:
+            try:
+                coasters_pair_amount = int(input("Please enter the number of coaster pairs you want :"))
+                break
+            except ValueError:
+                print("this is not a valid number, try again")
+        if coasters_pair_amount < 1:
+            print("please input a positive amount of pairs")
+        else:
             break
-        except ValueError:
-            print("this is not a valid number, try again")
-    if coasters_pair_amount < 1:
-        print("please input a positive amount of pairs")
-    else:
-        break
 
-for i in range(coasters_pair_amount):
-    Coaster = Coasters_overlap.Coasters_overlap()
-    s = Coaster.to_string()
-    r = str(Coaster.get_radius())
-    L = str(Coaster.get_length())
-    s_elem = ET.SubElement(element1, f'item')  # adding subtag under the 'items'
-    r_elem = ET.SubElement(s_elem, 'r')
-    l_elem = ET.SubElement(s_elem, 'l')
-    r_elem.text = r
-    l_elem.text = L
-    s_elem.text = str(i + 1)  # adding text between the 'item{i+1}
+    for i in range(coasters_pair_amount):
+        Coaster = Coasters_overlap.Coasters_overlap()
+        s = Coaster.to_string()
+        r = str(Coaster.get_radius())
+        L = str(Coaster.get_length())
+        s_elem = ET.SubElement(element1, f'item')  # adding subtag under the 'items'
+        r_elem = ET.SubElement(s_elem, 'r')
+        l_elem = ET.SubElement(s_elem, 'l')
+        r_elem.text = r
+        l_elem.text = L
+        s_elem.text = str(i + 1)  # adding text between the 'item{i+1}
 
-b_xml = ET.tostring(data)  # Converting the xml data to byte object, for allowing flushing data to file stream
-# with open("output.xml", "wb") as f:  # Opening a file, with operation mode `wb` (write + binary)
-#     f.write(b_xml)
+    b_xml = ET.tostring(data)  # Converting the xml data to byte object, for allowing flushing data to file stream
+    # with open("output.xml", "wb") as f:  # Opening a file, with operation mode `wb` (write + binary)
+    #     f.write(b_xml)
+
+    file2.write(b_xml)
+    print("Done! Check your output.xml file for the results")
 
 
-file2.write(b_xml)
-
-
-print("Done! Check your output.xml file for the results")
+if __name__=="__main__":
+    main()
